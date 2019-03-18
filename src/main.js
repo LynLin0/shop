75eab6ac991ca80fac950edab74f9416efa20bf9
lynlin0 <812274843@qq.com>
2019-03-19 00:30:49 +0800

    添加响应式弹性布局

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'amfe-flexible'
 import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false
-const scale = 1 / devicePixelRatio
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no')
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
